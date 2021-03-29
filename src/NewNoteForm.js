7b377e210306467ec13e81f341e0daf03c89d177
Elijah Prince <elijahprince73@gmail.com>
2021-03-29 14:18:10 -0700

    mobx stuff

---
+++ src/NewNoteForm.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import { useNotesStore } from './NotesContext'
+
+export const NewNoteForm = () => {
+  const [noteText, setNoteText] = useState()
+  const notesStore = useNotesStore()
+
+  return (
+    <>
+      <input 
+        value={noteText} 
+        onChange={(e) => setNoteText(e.target.value)} 
+        type="text" 
+      />
+      <button onClick={() => notesStore.addNote(noteText)} >Add Note</button>
+    </>
+  )
+}
